c02eecb9057173c86878723e08770b523df2fd9d
Luiz Gustavo do Amaral Correa <gustavo.lgac@gmail.com>
2024-12-04 16:42:40 -0300

    .

--- src/main/java/com/verfut/front/verfut/front/configuration/WebClientConfig.java
+++ src/main/java/com/verfut/front/verfut/front/configuration/WebClientConfig.java
@@ -9,6 +9,7 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient(WebClient.Builder builder){
-        return builder.baseUrl("http://localhost:8080/api").build();
+        return builder.baseUrl("http://44.202.41.115:8080/api").build();
+//        return builder.baseUrl("http://localhost:8080/api").build();
     }
 }
